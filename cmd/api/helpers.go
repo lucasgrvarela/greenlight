69e3ee50f24620b225728618667b85f67cd582bf
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2024-02-18 14:01:09 -0300

    send emails in background and recover from any panic

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -134,3 +134,17 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 
 	return i
 }
+
+func (app *application) background(fn func()) {
+	// Launch a background goroutine
+	go func() {
+		// Recover any panic and log error msg
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.Error(fmt.Sprintf("%v", err))
+			}
+		}()
+		// Execute the function we passed as param
+		fn()
+	}()
+}
